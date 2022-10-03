29c87243664c1025380db9f1f0d5ccd7ed5a00ac
Naelle69 <113608387+Naelle69@users.noreply.github.com>
2022-10-03 11:02:27 +0200

    EXERCICE PYTHON

---
+++ EXERCICEPYTHON1.py
@@ -0,0 +1,64 @@
+from ast import main
+import numbers
+from re import S
+from tkinter.tix import INTEGER
+
+#Exo 1:
+
+number1=1000
+number2=500
+number3=450
+#faire la somme des variables 
+somme=(number1+number2+number3)
+s=somme/3
+#faire la moyenne de somme
+print(s)
+#effectue la moyenne de ces trois nombres
+
+
+#exo 2:
+#ecrire un nombre entier
+
+number=int (input("saisir un nombre entier">=0))
+if number>=0:
+    print("Ce nombre est positif ou nul")
+else: 
+    print("Ce nombre est negatif")
+
+
+#exo 3:
+
+number=int(input("saisissez un nombre"))
+if a%3:
+    print("ce nombre est divisible par 3")
+else:
+    print("ce nombre n'est pas divisible par 3")
+
+
+#exo 4:
+
+number= int(input("saisissez un nombre"))
+Nhaut=a
+Nbas=b
+a-b=C
+
+
+while:
+if (Nhaut-Nbas<0.001):
+    break
+
+while(Nhaut-Nbas):      
+    if (c<0.001):
+        break
+    print("La racine number est C")
+
+    else (c!=0.001):
+    print("c n'est pas la racine carée de number")
+    
+
+
+
+
+
+
+
